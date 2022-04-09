aae54f15de9c37dc29285d04fdf2cd280b81eda8
gayathri u <gayathriu64@gmail.com>
2022-04-09 20:58:34 +0530

    layout change

--- src/App.js
+++ src/App.js
@@ -2,12 +2,10 @@ import { Route, Routes } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetUp";
 import FavouritePage from "./pages/Favourites";
 import NewMeetupPage from "./pages/NewMeetUp";
-import MainNavigation from "./components/Layout/MainNavigation";
-
+import Layout from "./components/layout/Layout";
 function App() {
   return (
-    <div>
-      <MainNavigation />
+    <Layout>
       <Routes>
         <Route path="/" element={<AllMeetupsPage />}></Route>
 
@@ -15,7 +13,7 @@ function App() {
 
         <Route path="/favourites" element={<FavouritePage />}></Route>
       </Routes>
-    </div>
+    </Layout>
   );
 }
 
